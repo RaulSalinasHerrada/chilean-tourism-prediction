c8d0a856812f7392d275bb6a5da79ee1d2e1767c
Raul Salinas <rasalina@uc.cl>
2024-07-20 11:28:50 -0400

    requirements for docker

--- app/utils.py
+++
@@ -1,9 +0,0 @@
-import pickle as pkl
-
-def save_object(object, path:str):
-    with open(path, "wb") as file:
-        pkl.dump(object, file)
-
-def load_object(path: str):
-    with open(path, "rb") as file:
-        return pkl.load(file)
